9ee45db01a7b8bca98a20282bd37adedbf3a57d7
Ariel <ariel50515@gmail.com>
2022-12-28 18:38:06 +0800

    todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from "react";
+import { ACTIONS } from "./Actions";
+import "./App.css";
+import Todo from "./Todo";
+
+const reducer = (todos, action) => {
+  console.log(todos, action);
+  const { todoContent, id } = action.payload;
+  switch (action.type) {
+    case ACTIONS.ADD:
+      return [...todos, newTodo(todoContent)];
+    case ACTIONS.TOGGLE:
+      return todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, complete: !todo.complete };
+        }
+        return todo;
+      });
+    case ACTIONS.DELETE:
+      return todos.filter((todo) => todo.id !== id);
+    default:
+      return todos;
+  }
+};
+
+const newTodo = (todoContent) => {
+  return {
+    id: Math.floor(Math.random() * 100000),
+    todoContent,
+    complete: false,
+  };
+};
 
 function App() {
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [todoContent, setTodoContent] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD, payload: { todoContent: todoContent } });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todoContent}
+          onChange={(e) => setTodoContent(e.target.value)}
+          placeholder="Write A To Do List"
+        />
+      </form>
+
+      {todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      ))}
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { ACTIONS } from "./Actions";
+
+const Todo = ({ todo, dispatch }) => {
+  return (
+    <div className="todo">
+      <span style={{ textDecoration: todo.complete ? "line-through" : null }}>
+        {todo.todoContent}
+      </span>
+      <button
+        className="toggle"
+        onClick={() =>
+          dispatch({ type: ACTIONS.TOGGLE, payload: { id: todo.id } })
+        }
+      >
+        {todo.complete ? "Cancel" : "Complete"}
+      </button>
+      <button
+        className="delete"
+        onClick={() =>
+          dispatch({ type: ACTIONS.DELETE, payload: { id: todo.id } })
+        }
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default Todo;
